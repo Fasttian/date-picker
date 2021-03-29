3b0e080da1724cda9131a56ebbc24859cc56dbd4
tianjiaxing <tianjiaxing2016@163.com>
2021-03-29 20:25:14 +0800

    picker layout

--- pages/date-picker/date-picker.vue
+++ pages/date-picker/date-picker.vue
@@ -1,5 +1,8 @@
 <template>
-	<view style="padding: 20px;">
+	<view style="padding: 20px; width: 300px;">
+		<view>单选</view>
 		<uni-date-picker />
+		<view style="margin-top: 30px;">范围选</view>
+		<uni-date-picker type="daterange" />
 	</view>
 </template>
--- uni_modules/uni-date-picker/components/uni-date-picker/uni-date-picker.vue
+++ uni_modules/uni-date-picker/components/uni-date-picker/uni-date-picker.vue
@@ -1,30 +1,75 @@
 <template>
 	<view class="uni-date">
-		<view v-if="!range" class="uni-date-x uni-date-single mt20">
-			<view class="uni-date__icon-logo"><uni-icons type="list" color="#666"></uni-icons></view>
-			<input class="uni-date__input" type="text" :value="displayValue" :placeholder="placeholder" />
-			<view class="uni-date__icon-clear"><uni-icons type="clear" color="#e1e1e1" size="14"></uni-icons></view>
+		<view class="uni-date-editor--X" @click="show">
+			<view v-if="!range" class="uni-date-x uni-date-single mt20">
+				<view class="uni-date__icon-logo">
+					<uni-icons type="list" color="#666"></uni-icons>
+				</view>
+				<input class="uni-date__input" type="text" :value="displayValue" :placeholder="placeholder" />
+				<view class="uni-date__icon-clear" @click="clear">
+					<uni-icons type="clear" color="#e1e1e1" size="14"></uni-icons>
+				</view>
+			</view>
+			<view v-else class="uni-date-x uni-date-range mt20">
+				<view class="uni-date__icon-logo">
+					<uni-icons type="list" color="#666"></uni-icons>
+				</view>
+				<input class="uni-date__input uni-date-range__input" type="text" :value="displayValue"
+					:placeholder="placeholder" />
+				<slot>
+					<view class="">{{rangeSeparator}}</view>
+				</slot>
+				<input class="uni-date__input uni-date-range__input" type="text" :value="displayValue"
+					:placeholder="placeholder" />
+				<view class="uni-date__icon-clear" @click="clear">
+					<uni-icons type="clear" color="#e1e1e1" size="14"></uni-icons>
+				</view>
+			</view>
 		</view>
-		<view  class="uni-date-x uni-date-range mt20">
-			<view class="uni-date__icon-logo"><uni-icons type="list" color="#666"></uni-icons></view>
-			<input class="uni-date__input uni-date-range__input" type="text" :value="displayValue" :placeholder="placeholder" />
-			<slot>
-				<view class="">{{rangeSeparator}}</view>
-			</slot>
-			<input class="uni-date__input uni-date-range__input" type="text" :value="displayValue" :placeholder="placeholder" />
-			<view class="uni-date__icon-clear"><uni-icons type="clear" color="#e1e1e1" size="14"></uni-icons></view>
+
+		<view ref="datePicker" v-if="popup" class="uni-date-picker__container" @click="show">
+<!-- 			<view class="uni-date-single--x" :style="popover">
+				<uni-calendar :selected="info.selected" :showMonth="false" @change="change"
+					@monthSwitch="monthSwitch" />
+			</view> -->
+			
+			<view class="uni-date-range--x" :style="popover">
+				<!-- <view class="uni-date-mask" @click="open"></view> -->
+				<uni-calendar :selected="info.selected" :showMonth="false" @change="change" @monthSwitch="monthSwitch" style="padding-right: 16px; border-right: 1px solid #F1F1F1;"/>
+				<uni-calendar :selected="info.selected" :showMonth="false" @change="change" @monthSwitch="monthSwitch" style="padding-left: 16px;" />
+			</view>
 		</view>
 	</view>
 </template>
 
+
+
 <script>
+	/**
+	 * 获取任意时间
+	 */
+	
 	export default {
 		data() {
 			return {
-				range: false
+				range: false,
+				visible: false,
+				popup: false,
+				popover: null,
+				displayValue: '',
+				info: {
+					lunar: true,
+					range: true,
+					insert: false,
+					selected: []
+				}
 			}
 		},
 		props: {
+			value: {
+				type: [String, Number],
+				default: ''
+			},
 			placeholder: {
 				type: String,
 				default: ''
@@ -34,12 +79,71 @@
 			}
 		},
 		computed: {
-			displayValue() {
-				return 123
-			}
+
 		},
+
 		methods: {
 
+			show(event) {
+				if (this.disabled || false) {
+					return
+				}
+				// this.valueChangeSource = ''
+				// console.log(11111, this.$refs.datePicker);
+				// const $datePicker = this.$refs.datePicker
+				// $picker.remove();
+				// (document.querySelector('uni-app') || document.body).appendChild(this.$refs.datePicker)
+				// this.$refs.datePicker.style.display = 'block'
+				const dateEditor = uni.createSelectorQuery().in(this).select(".uni-date-editor--X")
+				dateEditor.boundingClientRect(rect => {
+					this.popover = {
+						top: rect.top + rect.height + 50 + 'px',
+						left: rect.left + 'px',
+						// width: rect.width,
+						// height: rect.height
+					}
+				}).exec()
+				this.popup = !this.popup
+				// setTimeout(() => {
+				// 	this.visible = true
+				// }, 20)
+			},
+
+
+			change(e) {
+				// console.log('change 返回:', e)
+				this.displayValue = e.fulldate
+				// 模拟动态打卡
+				if (this.info.selected.length > 5) return
+				this.info.selected.push({
+					date: e.fulldate,
+					info: '打卡'
+				})
+			},
+			
+			clear() {
+				this.displayValue = ''
+				console.log(4444, this.displayValue);
+			},
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			confirm(e) {
+				console.log('confirm 返回:', e)
+			},
+			monthSwitch(e) {
+				console.log('monthSwitchs 返回:', e)
+			}
 		}
 	}
 </script>
@@ -55,17 +159,61 @@
 		background-color: #fff;
 		color: #666;
 	}
-	
+
 	.uni-date__input {
 		height: 40px;
 		width: 100%;
 		padding: 0 8px;
 		line-height: 40px;
 	}
+
 	.uni-date-range__input {
 		text-align: center;
 	}
+
+	.uni-date-picker__container {
+		/* display: none; */
+		position: fixed;
+		left: 0;
+		right: 0;
+		top: 0;
+		bottom: 0;
+		box-sizing: border-box;
+		z-index: 996;
+		font-size: 16px;
+	}
+
+	.uni-date-mask {
+		position: fixed;
+		bottom: 0px;
+		top: 0px;
+		left: 0px;
+		right: 0px;
+		background-color: rgba(0, 0, 0, 0.4);
+		transition-duration: 0.3s;
+		z-index: 996;
+	}
+
+	.uni-date-single--x {
+		position: absolute;
+		top: 0;
+		left: 0;
+		z-index: 999;
+		width: 375px;
+		border: 1px solid #F1F1F1;
+	}
 	
+	.uni-date-range--x {
+		display: flex;
+		position: absolute;
+		top: 0;
+		left: 0;
+		z-index: 999;
+		width: 733px;
+		border: 1px solid #F1F1F1;
+		border-radius: 4px;
+	}
+
 	.mt20 {
 		margin-top: 20px;
 	}
